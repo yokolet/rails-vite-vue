62515a672f73920745bf9400f219168954ddd74d
Yoko Harada <yokolet@gmail.com>
2024-03-04 20:57:03 +0900

    creates a Vue sample app

--- app/frontend/entrypoints/application.js
+++ app/frontend/entrypoints/application.js
@@ -26,3 +26,9 @@ console.log('Visit the guide for more information: ', 'https://vite-ruby.netlify
 
 // Example: Import a stylesheet in app/frontend/index.css
 // import '~/index.css'
+
+import { createApp } from 'vue'
+import './style.css'
+import App from '../App.vue'
+
+createApp(App).mount('#app')
